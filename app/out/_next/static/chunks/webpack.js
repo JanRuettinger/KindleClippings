5329545f66403acceb9111c953155c31c7190f10
Jan Ruettinger <service+github@janruettinger.com>
2020-12-01 16:12:14 +0100

    Deploy Next.js to gh-pages

--- app/out/_next/static/chunks/webpack.js
+++ app/out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ba64a5129f88e7e99987";
+/******/ 	var hotCurrentHash = "ce144727e1ad8cbe6021";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
