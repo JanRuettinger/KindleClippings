b1c975286522685d7ed4f2c42fc4027457cf62d6
Jan Ruettinger <service+github@janruettinger.com>
2020-12-02 15:33:21 +0100

    Deploy Next.js to gh-pages

---
+++ _next/static/OX6H8vgxX58L90pYC8MBh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-577f0f3c62958f1ca807.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-1e5eef79001b48be7153.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
